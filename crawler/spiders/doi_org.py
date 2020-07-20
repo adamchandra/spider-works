c1197be96b6a908aeb19f4e28aa305a17fa94eba
Adam Saunders <saunders@cs.umass.edu>
2020-07-20 10:50:17 -0400

    whitespace

--- crawler/spiders/doi_org.py
+++ crawler/spiders/doi_org.py
@@ -2,13 +2,13 @@
 import scrapy
 
 
-class DoiOrgSpider(scrapy.Spider):
-    name = 'doi_org'
-    allowed_domains = ['doi.org']
-    # start_urls = ['http://doi.org/']
-
-    def parse(self, response):
-        pass
+# class DoiOrgSpider(scrapy.Spider):
+#     name = 'doi_org'
+#     allowed_domains = ['doi.org']
+#     # start_urls = ['http://doi.org/']
+#
+#     def parse(self, response):
+#         pass
 
 
 # General scraper
